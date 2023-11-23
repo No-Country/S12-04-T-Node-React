43d18533109eb2e01ecabf360ba5577fe1489bc0
Enuel <enuel.monzon@gmail.com>
2023-11-23 20:26:52 -0300

    added global style config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,50 @@
 
 function App() {
   return (
-    <>
-      <h1>S12-04-T-Node-React</h1>
-    </>
+    <div
+      style={{
+        padding: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+      }}
+    >
+      <h1>--font-primary h1,h2 y h3 (títulos y subtítulos de ejemplo)</h1>
+      <p>`--font-secondary` Fuente secundaria, heredada de body</p>
+
+      <div style={{ display: "flex", gap: "2rem" }}>
+        <div
+          style={{ height: "200px", width: "200px", padding: "2rem" }}
+          className="bg-primary"
+        >
+          <p className="cl-secondary">bg-pramiry + cl-secondary</p>
+        </div>
+
+        <div
+          style={{ height: "200px", width: "200px", padding: "2rem" }}
+          className="bg-secondary"
+        >
+          <p className="cl-primary">bg-secondary + cl-primary</p>
+        </div>
+      </div>
+
+      <div
+        style={{ display: 'flex', gap:"2rem" }}
+      >
+        <div
+          style={{ height: "200px", width: "200px", padding: "2rem" }}
+          className="bs-light"
+        >
+          bs-light
+        </div>
+        <div
+          style={{ height: "200px", width: "200px", padding: "2rem" }}
+          className="bs-dark"
+        >
+          bs-dark
+        </div>
+      </div>
+    </div>
   );
 }
 
