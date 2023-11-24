b6194fe88300f122fd7bca48664b84f8af156e47
Enuel <enuel.monzon@gmail.com>
2023-11-24 19:35:18 -0300

    added dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@
 function App() {
   return (
     <div
-      style={{
-        padding: "2rem",
-        display: "flex",
-        flexDirection: "column",
-        gap: "2rem",
-      }}
+      className="p-5 flex flex-col gap-4"
     >
-      <h1>--font-primary h1,h2 y h3 (títulos y subtítulos de ejemplo)</h1>
-      <p>`--font-secondary` Fuente secundaria, heredada de body</p>
+      <h1 style={{ textAlign: "center" }} className="text-2xl bg-red-500 p-6 hover:bg-yellow-500 md:text-sm">
+        --font-primary h1,h2 y h3 (títulos y subtítulos de ejemplo)
+      </h1>
+      <p className="p-[10px] my-[2rem] cl-primary font-bold">
+        `--font-secondary` Fuente secundaria, heredada de body
+      </p>
 
       <div style={{ display: "flex", gap: "2rem" }}>
         <div
@@ -28,9 +27,7 @@ function App() {
         </div>
       </div>
 
-      <div
-        style={{ display: 'flex', gap:"2rem" }}
-      >
+      <div style={{ display: "flex", gap: "2rem" }}>
         <div
           style={{ height: "200px", width: "200px", padding: "2rem" }}
           className="bs-light"
@@ -43,6 +40,9 @@ function App() {
         >
           bs-dark
         </div>
+      </div>
+      <div className="h-[200px] bg-red-600 w-[200px] absolute right-2 bottom-2 flex justify-center items-center hover:text-white">
+        <h1>Hola</h1>
       </div>
     </div>
   );
