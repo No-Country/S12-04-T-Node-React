30729722ba03a4c295260014d4481c520a065b34
Ariel Martinez <arielstereo@msn.com>
2023-11-27 17:03:09 -0300

    Home page image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const backgroundImage = "url('/src/assets/images/header-img.jpg')";
+
   return (
-    <header className="flex flex-col items-center gap-32 justify-center mt-32">
-      <h1 className="text-6xl font-bold">Home</h1>
-      <Link to="/chat" className="text-slate-100 hover:text-blue-400 bg-red-600 p-4 rounded-xl">
-        ChatBot
-      </Link>
-    </header>
+    <>
+      <div
+        className="bg-cover bg-center h-screen flex items-center justify-center relative"
+        style={{
+          backgroundImage,
+          height: "calc(100vh - 80px)", // Calcula la altura restando 80px
+        }}
+      >
+        {/* Contenedor con blur */}
+        <Link to="chat">
+          <div className="bg-opacity-25 backdrop-filter p--0 backdrop-blur-md p-8  text-white text-center border-4 rounded-3xl">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
+              Conversa con el chef!
+            </h1>
+            <p className="mt-2 text-left text-base sm:text-lg md:text-xl lg:text-2xl">
+              Cocina con lo tengas en el momento
+            </p>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
