60096fd0aba523305c59b59f5180e6e83d473084
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-11-28 02:08:16 -0300

    responsive v1

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -55,8 +55,8 @@ export const Navigation = () => {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="relative flex h-16 items-center ">
+              <div className="justify-around sm:hidden">
                 {/* Botón menú celular */}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -71,7 +71,7 @@ export const Navigation = () => {
               <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center gap-10">
                   {/* LOGO APP */}
-                  <div>
+                  <div className="relative left-10">
                     <Link to="/" className="flex flex-col">
                       <img
                         className="h-8 w-auto"
