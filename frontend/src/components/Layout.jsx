37c4abc8c83a28a8ddd568a4b28af6ac44ff0756
Enuel <enuel.monzon@gmail.com>
2023-11-29 14:30:39 -0300

    fixes navigation & added useLocation

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,22 +1,28 @@
-import {Outlet} from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Navigation } from './Navigation'
+import NavbarResponsive from './NavbarResponsive';
+
 
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
-    <div className='container-fluid'>
-      <div >
+    <div className="container-fluid relative">
+      {location.pathname === "/" ? (
+        <NavbarResponsive />
+      ) : (
         <Navigation />
-      </div>
-      <Outlet  />
-      
+      )}
+      <Outlet />
+
       {/* ACA PODRIA IR EL FOOTER */}
-      
+
       {/* <div>
           <Footer />
           </div> */}
     </div>
-  )
+  );
 }
 
 export default Layout
---
+++ frontend/src/components/NavbarResponsive.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import hatLogo from "../assets/images/hat-logo.svg";
+import { Navigation } from "./Navigation";
+
+export default function NavbarResponsive() {
+  return (
+    <>
+      <div className="hidden sm:block">
+        <Navigation />
+      </div>
+      <div className="sm:hidden absolute top-3 left-0 w-full z-10">
+        <div className="flex relative">
+          <button className="absolute left-5 top-2 px-3 py-2 rounded-full bg-opacity-25 backdrop-filter p--0 backdrop-blur-md bg-gray-700">
+            <Link to="/" className="flex items-center gap-1">
+              <img className="h-6 w-auto" src={hatLogo} alt="Logo App" />
+              <h1 className="text-white">Chef GPT</h1>
+            </Link>
+          </button>
+          {/* <div className="absolute right-5 top-1 p-2">
+            <img
+              className="h-8 w-8 rounded-full border-2"
+              src={userLogo}
+              alt=""
+            />
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+}
