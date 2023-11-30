b95199c2a86b45b50a4dbf5fd2bcee464137b064
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-11-30 17:37:04 -0300

    Se acomodo los scroll de la page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -29,14 +29,16 @@ const Login = () => {
   }
   return (
     <>  
-      <img src={img} alt="img" className="w-[10rem] mx-auto pt-4 mt-16"/>
-      <h1 className="text-7xl font-normal text-center my-4">BIENVENIDO</h1>
-      <h3 className='text-center text-3xl font-bold my-4' style={{fontFamily: 'SF Pro Display'}}>Iniciar Sesion</h3>
-      <form action="" onSubmit={handleData} className="flex flex-col sm:w-1/2  justify-around p-4 mx-auto ">
+    <div className='bg-[#F9E9E7] h-screen flex flex-col bg-center items-center justify-center '  >
+
+      <img src={img} alt="img" className="w-[11rem] mx-auto pt-4 mt-10"/>
+      <h1 className="text-3xl font-normal text-center my-4">BIENVENIDO</h1>
+      <h3 className='text-center text-2xl font-bold mb-2' style={{fontFamily: 'SF Pro Display'}}>Iniciar Sesion</h3>
+      <form action="" onSubmit={handleData} className="flex flex-col sm:w-1/2  justify-around mx-auto ">
         
         <input 
         style={{backgroundColor: 'rgba(118, 120, 118, 0.46)', borderRadius: '10px'}}
-        className='min-w-[12rem] ps-2 text-2xl py-6 mb-4 placeholder:bolder placeholder:text-black placeholder:text-900'
+        className='min-w-[12rem] ps-2 text-2xl py-2 mb-4 placeholder:bolder placeholder:text-black placeholder:text-900'
         type="text" 
         placeholder="Usuario..."
         name="username"
@@ -49,16 +51,16 @@ const Login = () => {
         <input 
         style={{backgroundColor: 'rgba(118, 120, 118, 0.46)',
         borderRadius: '10px'}}
-        className='min-w-[12rem] ps-2 text-2xl py-6 mt-4 placeholder:bolder placeholder:text-black placeholder:text-900 '
+        className='min-w-[12rem] ps-2 text-2xl py-2 mt-4 placeholder:bolder placeholder:text-black placeholder:text-900 '
         type="password" 
         name="password"
         placeholder='Contraseña...'
         value={dataUser.password} 
         onChange={(e) => setDataUser({ ...dataUser, password: e.target.value })}
         />
-        <Link to="/" className="text-xl font-bold mt-4" style={{fontFamily: 'SF Pro Display'}}>¿Olvidaste tu contraseña?</Link>
+        <Link to="/" className="text-xl font-bold mt-2" style={{fontFamily: 'SF Pro Display'}}>¿Olvidaste tu contraseña?</Link>
 
-        <p className='text-xl font-bold mt-4 text-center'>No tienes un usuario?</p>
+        <p className='text-xl font-bold mt-2 text-center'>No tienes un usuario?</p>
         <Link to="/register" className="text-xl font-bold mb-4 text-center underline" style={{fontFamily: 'SF Pro Display'}}>Registrate</Link>
 
 
@@ -73,6 +75,7 @@ const Login = () => {
 
 
       </form>
+    </div>
     </>
   )
 }
