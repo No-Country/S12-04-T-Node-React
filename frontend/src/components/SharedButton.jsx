3307eea799ae8223525d8553bf013f6266fdddba
Ariel Martinez <arielstereo@msn.com>
2023-12-02 21:17:33 -0300

    add url to wsp

--- frontend/src/components/SharedButton.jsx
+++ frontend/src/components/SharedButton.jsx
@@ -1,16 +1,15 @@
 import { MdOutlineWhatsapp } from "react-icons/md";
-import {useNavigate, useHistory} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 // eslint-disable-next-line react/prop-types
 const SharedButton = ({ phone, text }) => {
 
 const navigate = useNavigate();
-const history = useHistory();
 
   const handleClick = () => {
     const encoded = encodeURIComponent(text);
     const url = `https://wa.me/${phone}?text=${encoded}`;
-    history.push(url);
+    window.open(url, '_blank');
     navigate("/")
   };
 
