b8d8aebe1305bf76080d07358f09426d88e57070
Ariel Martinez <arielstereo@msn.com>
2023-12-15 16:19:01 -0300

    update favoriteCard

--- frontend/src/components/FavoriteCard.jsx
+++ frontend/src/components/FavoriteCard.jsx
@@ -1,36 +1,42 @@
-
 import lapiz from "../assets/images/lapiz.svg";
 import tacho from "../assets/images/tacho.svg";
 import useRecipeStore from "../store/useRecipeStore";
-function FavoriteCard({ title, description, categoria }) {
 
+function FavoriteCard({ title, recipe, category }) {
   const deleteFavorite = useRecipeStore((state) => state.deleteFavorite);
-  function deleteRecipe(){
-      deleteFavorite(title)      
+  function deleteRecipe() {
+    deleteFavorite(title);
   }
-  function editRecipe(){  
-  }
-
+  function editRecipe() {}
 
   return (
     <div
       className={
-        categoria +
+        category +
         " bg-[#D9D9D9] border-rounded rounded ps-7 py-5 mb-4 relative"
       }
     >
-      <div className="flex absolute top-5 right-5 gap-5" >
-        <img src={lapiz} className="hover:cursor-pointer"  onClick={editRecipe} alt="Editar" />
-        <img src={tacho} className="hover:cursor-pointer" onClick={deleteRecipe}  alt="Eliminar" />
+      <div className="flex absolute top-5 right-5 gap-5">
+        <img
+          src={lapiz}
+          className="hover:cursor-pointer"
+          onClick={editRecipe}
+          alt="Editar"
+        />
+        <img
+          src={tacho}
+          className="hover:cursor-pointer"
+          onClick={deleteRecipe}
+          alt="Eliminar"
+        />
       </div>
 
       <div>
         <h2 className="card-title" style={{ textTransform: "capitalize" }}>
           {title}
         </h2>
-
-        <p>{description}</p>
-
+        <p>{category}</p>
+        <p>{recipe}</p>
       </div>
     </div>
   );
